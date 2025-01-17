14bbcf74156a965e4c7fe8821e82af4d6ddfbb4c
charliekaks <kaksunchained@gmail.com>
2025-01-17 16:37:44 +0300

    created a player class that inherits from circleshape class and has functionality to move the player around in the game.

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 from constants import *
+from player import *
 
 
 def main():
@@ -10,15 +11,22 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     game_time = pygame.time.Clock()
     dt = 0
+    x = SCREEN_WIDTH/2
+    y = SCREEN_HEIGHT/2
+    player = Player(x,y)
     while(True):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+
+        player.draw(screen)
+        player.update(dt)
         pygame.display.flip()
         # limit the framerate to 60 FPS
         time_pass = game_time.tick(60)
         dt = time_pass/1000
+        
 
 
 
