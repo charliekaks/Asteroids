a8312d2ef64ce7756ea49093f58b6c464fe62ba6
charliekaks <kaksunchained@gmail.com>
2025-02-11 13:00:03 +0300

    Adding collision logic to the fired shots on asteroids.

--- main.py
+++ main.py
@@ -44,6 +44,11 @@ def main():
             if asteroid.check_collisions(player):
                 print("Game over!")
                 sys.exit()
+
+            for shot in shots:
+                if asteroid.check_collisions(shot):
+                    shot.kill()
+                    asteroid.kill()
         
         screen.fill("black")
 
